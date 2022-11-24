62ce60eec8b316f31a76b4cb1f02905e5de8bb3a
Jelte ten Holt <inotherwords@gmail.com>
2022-11-24 13:05:35 +0100

    updated image component

--- views/components/input/image.blade.php
+++ views/components/input/image.blade.php
@@ -3,18 +3,25 @@
 <div class="max-w-md">
     <label class="mb-2 block text-base font-medium text-gray-700"> {{ Str::ucfirst($model->title) }} </label>
     <div class="@error($model->model) border-red-500 @enderror rounded-md border border-gray-300 bg-white p-8">
-        {{-- Image Preview --}}
-        @isset($model['content'])
-            <img src="{{ Storage::url($model['content']) }}" class="mb-8 w-full rounded-md" alt="">
-        @endisset
 
+        
         {{-- Image upload --}}
         <input
             wire:model.defer="{{ $model->model }}"
-            type="file">
+            type="file"
+        >
+
+        {{-- Image Preview --}}
+        @isset($model['content'])
+            <div class="mt-6">
+                <img src="{{ Storage::url($model['content']) }}" class="mb-8 w-full rounded-md" alt="">
+                <x-dashcomp::buttons.slot danger="true" type="button" wire:click="removeImage('{{$model->model}}')"> Remove Image </x-dashcomp::buttons.slot>
+            </div>
+        @endisset
 
         @error($model->model)
             <span class="block pt-2 text-red-500">{{ $message }}</span>
         @enderror
+
     </div>
 </div>
